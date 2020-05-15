f7af3fa058b9d71ec6bc63ea650b2bd9e236314f
zhaobao1830 <373098777@qq.com>
2020-05-15 08:55:53 +0800

    G2demo页面

---
+++ src/components/F2.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+     <canvas id="myChart" width="400" height="260"></canvas>
+  </div>
+</template>
+
+<script>
+  import F2 from '@antv/f2'
+
+  export default {
+    name: "F2",
+    mounted() {
+      this.initChart()
+    },
+    methods: {
+      initChart () {
+        const data = [
+          { genre: 'Sports', sold: 275 },
+          { genre: 'Strategy', sold: 115 },
+          { genre: 'Action', sold: 120 },
+          { genre: 'Shooter', sold: 350 },
+          { genre: 'Other', sold: 150 },
+        ]
+
+        const chart = new F2.Chart({
+          id: 'myChart',
+          pixelRatio: window.devicePixelRatio // 指定分辨率
+        })
+
+        chart.source(data)
+
+        chart
+          .interval()
+          .position('genre*sold')
+          .color('genre')
+
+        chart.render()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
